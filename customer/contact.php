1016d3c82900f545ad61a47453554a81612de4d9
lqie02 <tanleqie@gmail.com>
2023-01-11 15:46:25 +0800

    .
    .

--- customer/contact.php
+++ customer/contact.php
@@ -1,10 +1,28 @@
 <?php ob_start(); $title = "Menu"; include('header.php') ;?>
-
+<html>
+	<style>
+		h2{
+			margin-left: 160px;
+		}
+		p{
+			margin-left: 140px;
+		}
+	</style>
 <body class="location">
 	<div class="container">
 	  <div class="contact">
         <h2  class = "location-logo">CONTACT US</h2>
-        <p>More Info here<br/> Address Here<br/>Address line 2 <br/> Tel : 019232738 <br/> Email:admin@admin.com</p>
+		  <br>
+		  <p>More Info here</p>
+		  <br><br>
+		   <p>Address Here</p>
+		   <p>Lorong Bukit Beruang Utama 1, 75450 Ayer Keroh, Melaka </p>
+		  <br>
+		   <p>Tel : 019-5050053 </p>
+		  <br>
+		   <p>Email : <a href="mailto:laurentan2000@gmail.com"> laurentan2000@gmail.com</a></p>
+		  
 	  </div>
 	</div>
 </body>
+</html>
