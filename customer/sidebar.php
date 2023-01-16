2584f792125047f7a54f515821708fc554645377
AisyAtiqah <Aisyaatiqah@gmail.com>
2023-01-16 22:23:09 +0800

    .
    .

--- customer/sidebar.php
+++ customer/sidebar.php
@@ -3,12 +3,12 @@
 <title>W3.CSS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-metro.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-win8.css">
 <body>
 
 <!-- Sidebar -->
 <!-- /*Color sidebar*/ -->
-<div class="w3-sidebar w3-metro-dark-orange w3-bar-block" style="width:10%">
+<div class="w3-sidebar w3-win8-sienna w3-bar-block" style="width:10%">
   <h3><a href="viewmenu.php"  class="w3-bar-item w3-button">Menu</a></h3>
   <a href="sdrinks.php" class="w3-bar-item w3-button">Beverages</a>
   <a href="sdishes.php" class="w3-bar-item w3-button">Side Dishes</a>
